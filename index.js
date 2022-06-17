ffe1584c40907566f61ca80861e93774bb28aff0
ZichenYuan <y.zichen@wustl.edu>
2022-06-16 17:47:31 -0700

    first update

---
+++ index.js
@@ -0,0 +1,23 @@
+$(".portrait").hide().fadeIn(4000);
+$(".about").hide().fadeIn(4000);
+
+function cloud(element){
+
+  $(element).animate({ marginLeft:"+=50px",marginTop:"-=15px"
+}, 20000);
+  $(element).animate({ marginTop:"+=15px"}, 5000);
+
+  $(element).animate({ marginLeft:"-=50px"}, 20000);
+
+  $(element).animate({ marginLeft:"-=45px",marginTop:"+=15px"
+}, 20000);
+
+  $(element).animate({ marginTop:"-=15px"}, 5000);
+
+  $(element).animate({ marginLeft:"+=45px"}, 20000);
+}
+
+
+cloud(".cloud");
+cloud("h1");
+cloud(".portrait")
